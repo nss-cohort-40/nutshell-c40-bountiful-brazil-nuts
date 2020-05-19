ebb340d15c90cdce3e007cdde60254455b70f18b
zanebliss <zanebliss@icloud.com>
2020-05-18 20:00:21 -0500

    Merge branch 'master' of https://github.com/nss-cohort-40/nutshell-c40-bountiful-brazil-nuts

--- src/scripts/tasksDOM.js
+++
@@ -1,7 +0,0 @@
-import taskComponent from "./tasksComponent.js"
-
-function writeDOM () {
-    document.querySelector("#container").innerHTML = taskComponent.taskDOM()
-}
-
-export default { writeDOM }
