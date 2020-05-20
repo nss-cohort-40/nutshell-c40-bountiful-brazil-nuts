2331cbb3eb3065f4a13a8e26e2459f455e6ad732
zanebliss <zanebliss@icloud.com>
2020-05-20 15:10:59 -0500

    init commit

--- src/scripts/welcComp.js
+++ src/scripts/welcComp.js
@@ -1,8 +1,10 @@
 // WELCOME MESSAGE COMPONENT
 function makeWelcome() {
     return `
+    <div id="welcomeWrapper">
     <h1>Welcome to Nutshell</h1>
     <button id="populate--regForm">Register a new account</button>
+    </div>
     `
 }
 
