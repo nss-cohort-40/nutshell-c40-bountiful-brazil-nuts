faf0409c2ca31e79f320a4db977dbd8a17e0f55c
tannerb9 <tanner-brainard@hotmail.com>
2020-05-21 14:57:48 -0500

    save before checking fm pull request

--- src/scripts/chatData.js
+++ src/scripts/chatData.js
@@ -1,22 +1,4 @@
-// Chat Functionality:
-// chat container that displays messages from users
-// container has text input box to write message
-// message sent on enter keypress(charCode == 13)
-// message is displayed(username on left, message to right)
-// most recent messages displayed at bottom of container
-// container has scroll function to see older messages
-// edit affordance on each message(only viewable by user that sent it)
-// edit box appears(prepopulated) and text editing enabled
-// edits are saved by save affordance
-
-// TODO: Create html string template for container(id="chatBox")
-// TODO: Create html string template for messages(class="message") ??
-// TODO: Create text input for message
-// Enable keypress event to submit newMessage(charCode == 13)
-// POST newMessage to API
-// GET and render newMessage to chatBox(with username prepended)
-// TODO: Link userid to username and include in newMessage
-
+// Authors: Tanner & Zane Bliss
 const API = {
   getMessage(messageId) {
     return fetch(`http://localhost:8088/messages/${messageId}?_expand=user`).then(response => response.json());
